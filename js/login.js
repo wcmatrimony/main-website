85606f7c4a4947bf2a3b4f1c8d304434551867b7
unknown <deepakkv8335@gmail.com>
2026-09-10 22:28:38 +0530

    Added login system and protected dashboard

---
+++ js/login.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+
+    const { error } = await supabaseClient.auth.signInWithPassword({
+        email,
+        password
+    });
+
+    if (error) {
+        alert(error.message);
+        return;
+    }
+
+    window.location.href = "dashboard.html";
+});
